706249b6815cf69b5b40b9b393f99119d1e8b74d
Sarukrishna Ayyamperumal <sarukrishna.ayyamperumal@esko.com>
2025-01-11 18:54:34 +0530

    Monkey patch for pyrogram bug

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,16 @@
 from .helpers import unpack_new_file_id
 from .database import Media, save_file, get_search_results
+
+# Monkey patch for Pyrogram bug
+from pyrogram import utils
+
+def get_peer_type_new(peer_id: int) -> str:
+    peer_id_str = str(peer_id)
+    if not peer_id_str.startswith("-"):
+        return "user"
+    elif peer_id_str.startswith("-100"):
+        return "channel"
+    else:
+        return "chat"
+
+utils.get_peer_type = get_peer_type_new
